6e825be6d4b43fcf0ff9c1c34baf1bb6f735d4ca
DhiralKaniya <dhiralkaniya3146@gmail.com>
2017-06-26 10:20:37 +0530

    User module updated

--- AMS-Backend/User/user.js
+++ AMS-Backend/User/user.js
@@ -1,6 +1,7 @@
 var dbConnect = require('../DBFiles/dbConfig.js');
 var client = dbConnect.myconnection;
 var randonmString = require('randomstring');
+//register user
 var reg = function(request, response) {
     var username = request.params.username;
     var pin = request.params.pin;
@@ -17,6 +18,16 @@ var reg = function(request, response) {
     var groupid = request.params.groupid;
     var secid = request.params.secid;
 
+    var userexistQuery = "SELECT * FROM tbl_user where username = '" + username + "' OR pin = " + pin + " OR email = '" + email + "'";
+    client.query(userexistQuery, function(err, result) {
+        if (result.rowCount > 0 || err) {
+            var status = {
+                'status': false,
+                'error': 'user already exist'
+            };
+            response.json(status);
+        }
+    });
     var query1 = "INSERT INTO tbl_user(username,pin,firstname,lastname,contact1,contact2,gender,no_of_mem,flat_no,isactive,validtoken,email) values('" + username + "'," + pin + ",'" + firstname + "','" + lastname + "','" + contact1 + "','" + contact2 + "','" + gender + "'," + members + ",'" + flatno + "'," + true + ",'" + token + "','" + email + "') RETURNING userid";
     client.query(query1, function(err, result, fields) {
         if (err) {
@@ -59,7 +70,57 @@ var reg = function(request, response) {
         }
     });
 };
+//update user
+var upd = function(request, response) {
+    var username = request.params.username;
+    var pin = request.params.pin;
+    var firstname = request.params.firstname;
+    var lastname = request.params.lastname;
+    var contact1 = request.params.contact1;
+    var contact2 = request.params.contact2;
+    var gender = request.params.gender;
+    var email = request.params.email;
+    var members = request.params.member;
+    var flatno = request.params.flatno;
+    var email = request.params.email;
+    var tkn = randonmString.generate();
+    var groupid = request.params.groupid;
+    var userid = request.params.userid;
 
+    var query1 = "UPDATE tbl_user SET username='" + username + "' , pin='" + pin + "',firstname='" + firstname + "',lastname='" + lastname + "',contact1='" + contact1 + "',contact2='" + contact2 + "',gender='" + gender + "',no_of_mem=" + members + ",flat_no='" + flatno + "',isactive=" + true + ",validtoken='" + tkn + "',email='" + email + "'  WHERE userid=" + userid + "";
+    client.query(query1, function(err, result, fields) {
+        if (err) {
+            var status = {
+                'status': false,
+                'error place': 'update in user',
+                'error': err
+            }
+            response.json(status);
+        }
+    });
+    client.query("Update tbl_user_secretary_group SET groupid = " + groupid + " WHERE userid = " + userid + "", function(err, result) {
+        if (err) {
+            var status = {
+                'status': false,
+                'error place': 'update in user',
+                'error': err
+            }
+            response.json(status);
+        }
+    });
+    client.query("SELECT * FROM tbl_user where userid=" + userid + "", function(err, result) {
+        var user = result.rows[0];
+        var status = {
+            'status': true,
+            'user': result.rows[0]
+        }
+        response.json(status);
+    });
+};
+
+
+
+//authenticate user
 var auth = function(request, response) {
     var username = request.params.username;
     var pin = request.params.pin;
@@ -113,28 +174,29 @@ var auth = function(request, response) {
     })
 };
 
-
+//block user
 var block = function(request, response) {
-    var username = request.params.username;
-    var query = "UPDATE tbl_user SET isactive = false WHERE username = '" + username + "'";
-    client.query(query, function(err, result) {
-        if (err || result.rowCount <= 0) {
-            var status = {
-                'status': 'false',
-                'error place': 'block user error',
-                'err': err
+        var userid = request.params.userid;
+        var query = "UPDATE tbl_user SET isactive = false WHERE userid = '" + userid + "'";
+        client.query(query, function(err, result) {
+            if (err || result.rowCount <= 0) {
+                var status = {
+                    'status': 'false',
+                    'error place': 'block user error',
+                    'err': err
+                }
+            } else {
+                var status = {
+                    'status': 'true'
+                }
             }
-        } else {
-            var status = {
-                'status': 'true'
-            }
-        }
-        response.json(status);
-    })
-}
+            response.json(status);
+        })
+    }
+    //active user
 var active = function(request, response) {
-    var username = request.params.username;
-    var query = "UPDATE tbl_user SET isactive = true WHERE username = '" + username + "'";
+    var userid = request.params.userid;
+    var query = "UPDATE tbl_user SET isactive = true WHERE userid = '" + userid + "'";
     client.query(query, function(err, result) {
         if (err || result.rowCount <= 0) {
             var status = {
@@ -150,7 +212,31 @@ var active = function(request, response) {
         response.json(status);
     })
 }
+
+var secUser = function(request, response) {
+    var secretary = request.params.secid;
+    var query = "select * from tbl_user_secretary_group tsu,tbl_user tu WHERE tu.userid = tsu.userid and secretaryid=" + secretary + "";
+    console.log(query);
+    client.query(query, function(err, result) {
+        if (err || result.rowCount <= 0) {
+            var status = {
+                'status': false,
+                'error': 'error in sec User'
+            }
+            response.json(status);
+        } else {
+            var status = {
+                'status': true,
+                'user': result.rows
+            }
+            response.json(status);
+        }
+    });
+};
+
 module.exports.blockuser = block;
 module.exports.checkuser = auth;
 module.exports.registration = reg;
 module.exports.activeuser = active;
+module.exports.updateuser = upd;
+module.exports.secretaryuser = secUser;
